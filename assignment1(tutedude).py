9cf68d1d44674ed16377a9192d4429b3946fa16d
Aadesh Bhamare <bhamareaadesh3011@gmail.com>
2025-12-30 12:37:40 +0530

    Add files via upload

---
+++ assignment1(tutedude).py
@@ -0,0 +1,18 @@
+#TASK 1
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+Addition=num1+num2
+Subtraction=num1-num2
+Multiplication=num1*num2
+Division=num1/num2
+print("Addition:",Addition)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
+
+#TASK 2
+n = input("Enter first name: ")
+m = input("Enter last name: ")
+print("Hello,", n, m + "! Welcome to the Python program")
+
+
